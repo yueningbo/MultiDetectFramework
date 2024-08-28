3aad37c6735b492042ad1c60d3589e316557087f
ynb_pao <397460848@qq.com>
2024-08-29 00:21:42 +0800

    增加功能：过滤物体分类

--- scripts/pascal_voc_xml_to_coco_json.py
+++ scripts/pascal_voc_xml_to_coco_json.py
@@ -52,7 +52,7 @@ def add_annotation(coco, annotation_id, image_id, category_id, bbox):
     coco['annotations'].append(annotation_item)
 
 
-def parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_path):
+def parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_path, selected_categories=None):
     with open(os.path.join(txt_path, txt_file_name), 'r') as f:
         img_ids_set = {line.split()[0] for line in f.readlines()}
 
@@ -60,6 +60,7 @@ def parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_
     xml_files = os.listdir(xml_path)
 
     for i, xml_file in enumerate(xml_files):
+        print('------------------------------------------------')
         if not xml_file.endswith('.xml') or xml_file.split('.')[0] not in img_ids_set:
             if xml_file.split('.')[0] not in img_ids_set:
                 print(f'[{i + 1}/{len(xml_files)}]Processing file: {xml_file} not in {txt_file_name}')
@@ -76,6 +77,7 @@ def parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_
         size = {'width': None, 'height': None, 'depth': None}
         file_name = None
         current_image_id = None
+        image_valid = False  # 用于检查图像是否包含所选类别
 
         for elem in root:
             if elem.tag == 'filename':
@@ -95,6 +97,10 @@ def parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_
                 for subelem in elem:
                     if subelem.tag == 'name':
                         object_name = subelem.text
+                        if selected_categories and object_name not in selected_categories:
+                            continue  # 跳过不在所选类别中的对象
+
+                        image_valid = True  # 图像中包含有效的对象
                         current_category_id = category_set.get(object_name) or add_category(coco, category_set,
                                                                                             object_name)
 
@@ -102,7 +108,7 @@ def parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_
                         for option in subelem:
                             bndbox[option.tag] = int(option.text)
 
-                if None not in bndbox.values() and object_name:
+                if None not in bndbox.values() and object_name and object_name in selected_categories:
                     if current_image_id is None:
                         current_image_id = add_image(coco, image_set, file_name, size)
                         print(f'Added image: {file_name} with size {size}')
@@ -114,13 +120,16 @@ def parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_
                         bndbox['ymax'] - bndbox['ymin']
                     ]
                     annotation_id += 1
-                    print(f'Adding annotation: {object_name}, Image ID: {current_image_id}, '
+                    print(f'\tAdding annotation: {object_name}, Image ID: {current_image_id}, '
                           f'Category ID: {current_category_id}, Category: {object_name}, BBox: {bbox}')
-                    add_annotation(coco, annotation_id, current_image_id, current_category_id, bbox)
-        print('------------------------------------------------')
+                    if image_valid:
+                        add_annotation(coco, annotation_id, current_image_id, current_category_id, bbox)
+
+        if not image_valid:
+            print(f'Image {file_name} contains no selected categories, skipping...')
 
 
-def build_coco_annotations(xml_path, output_path, txt_file_name, txt_path):
+def build_coco_annotations(xml_path, output_path, txt_file_name, txt_path, selected_categories=None):
     coco = {
         'images': [],
         'type': 'instances',
@@ -130,7 +139,7 @@ def build_coco_annotations(xml_path, output_path, txt_file_name, txt_path):
     category_set = {}
     image_set = set()
 
-    parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_path)
+    parse_xml_files(xml_path, txt_file_name, coco, category_set, image_set, txt_path, selected_categories)
 
     with open(output_path, 'w') as f:
         json.dump(coco, f)
@@ -143,8 +152,10 @@ if __name__ == "__main__":
     txt_file_list = ['train.txt', 'test.txt', 'val.txt']
     output_path = "data/datasets/VOCdevkit/VOC2007"
 
+    selected_categories = ['dog', 'cat']  # 仅生成这些类别的注释
+
     for txt_file in txt_file_list:
         output_json_path = os.path.join(output_path, txt_file.replace('txt', 'json'))
         print(f'Building COCO annotations for {txt_file}...')
-        build_coco_annotations(xml_path, output_json_path, txt_file, txt_path)
+        build_coco_annotations(xml_path, output_json_path, txt_file, txt_path, selected_categories)
         print(f'COCO annotations saved to {output_json_path}')
